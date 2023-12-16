65ffaea502ef2eb5d990251e720f0aca36a4d8b5
Kyle Jacobson <jacokyle01@gmail.com>
2023-12-16 14:29:59 -0600

    setup esbuild with watching

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import Ctrl from './ctrl'
+import view from './view';
+
+import {
+    init,
+    eventListenersModule,
+    h,
+    propsModule,
+    attributesModule,
+    classModule
+  } from "snabbdom";
+  
+  const patch = init([
+    eventListenersModule,
+    propsModule,
+    attributesModule,
+    classModule
+  ]);
+
+const ctrl = new Ctrl(redraw);
+const element = document.getElementById("main");
+element.innerHTML = "";
+const inner = document.createElement("div");
+element.appendChild(inner);
+let vnode = patch(inner, view(ctrl));
+
+function redraw() {
+    vnode = patch(vnode, view(ctrl));
+}
